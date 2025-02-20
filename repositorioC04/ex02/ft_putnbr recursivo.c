cd36a4c1b95168a622ec78c31288ea030695d848
Mohammed Chahid Tijani <mochahid@c1r10s1.42malaga.com>
2025-02-20 23:32:19 +0100

    piscina42

---
+++ repositorioC04/ex02/ft_putnbr recursivo.c
@@ -0,0 +1,46 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putnbr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mochahid <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/19 18:46:02 by mochahid          #+#    #+#             */
+/*   Updated: 2025/02/19 19:42:04 by mochahid         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+#include <limits.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putnbr(int nb)
+{
+	if (nb == INT_MIN)
+	{
+		write(1, "-2147483648", 11);
+		return;
+	}
+	if (nb < 0)
+	{
+		nb = -nb;
+		ft_putchar('-');
+	}
+	if (nb >= 10)
+	{
+		ft_putnbr(nb / 10);
+		nb = nb % 10;
+	}
+	if (nb < 10)
+		ft_putchar(nb + '0');
+}
+
+int	main(void)
+{
+	int nb = -2147483648;
+	ft_putnbr (nb);
+}
